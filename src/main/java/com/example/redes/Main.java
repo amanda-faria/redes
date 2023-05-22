fdd58247984902b4914fc063942d1f8b75f25dd1
Lucas Alves <lucasalves.12@live.com>
2023-05-21 22:43:14 -0300

    vincular o back com a interface de usuário

--- src/main/java/com/example/redes/Main.java
+++ src/main/java/com/example/redes/Main.java
@@ -1,6 +1,12 @@
 package com.example.redes;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
 
 public class Main extends JFrame {
 
@@ -16,18 +22,54 @@ public class Main extends JFrame {
     private JLabel Assunto;
     private JLabel Mensagem;
     private JPanel MainPanel;
+    final String password = "lphinbiznhexqmgr";
 
-//    JTextField emailDestinatario = new JTextField();
 
-    public Main () {
-//        add(emailDestinatario);
-//        emailDestinatario.setText("lucasalves0495@gmail.com");
+    public Main() {
+
         setContentPane(MainPanel);
         setTitle("Simple Email APP");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800,500);
+        setSize(800, 500);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        System.out.println("Iniciando SSLEmail");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+        props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
+        props.put("mail.smtp.port", "465"); //SMTP Port
+
+        System.out.println("SSLEmail iniciado");
+
+        enviarEMailButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Authenticator auth = new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(textField2.getText(), password);
+                    }
+                };
+
+                Session session = Session.getDefaultInstance(props, auth);
+                System.out.println("A sessão foi criada");
+
+                if (enviarImagemCheckBox.isSelected() == true) {
+                    UtilidadeEmail.enviarEmailImagem(session, textField1.getText(), textField4.getText(), textField5.getText());
+                }
+
+                if (enviarArquivoCheckBox.isSelected() == true) {
+                    UtilidadeEmail.enviarEmailAnexo(session, textField1.getText(), textField4.getText(), textField5.getText());
+                }
+
+                if (enviarImagemCheckBox.isSelected() == false && enviarArquivoCheckBox.isSelected() == false) {
+                    UtilidadeEmail.enviarEmail(session, textField1.getText(), textField4.getText(), textField5.getText());
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
